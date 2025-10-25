3759db6ffa3ae862de3c12d00e2a332b17b5e8bb
ottolol <alexsveshnik@gmail.com>
2025-10-25 23:24:02 +0300

    RC-63 feat: add Tabs component

---
+++ src/shared/ui/tabs/Tabs.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useState } from 'react'
+import s from './Tabs.module.scss'
+
+interface Tab {
+  label: string
+  content: React.ReactNode
+  disabled?: boolean
+}
+
+interface TabsProps {
+  tabs: Tab[]
+  defaultActiveIndex?: number
+}
+
+export default function Tabs({ tabs, defaultActiveIndex = 0 }: TabsProps) {
+  const [activeIndex, setActiveIndex] = useState(defaultActiveIndex)
+
+  return (
+    <div className={s.tabs}>
+      <div className={s.tabsHeader} role="tablist">
+        {tabs.map((tab, index) => (
+          <button
+            id={`tab-${index}`}
+            key={index}
+            className={`${s.tabButton} ${index === activeIndex ? s.active : ''} ${tab.disabled ? s.disabled : ''}`}
+            onClick={() => !tab.disabled && setActiveIndex(index)}
+            disabled={tab.disabled}
+            role="tab"
+            aria-selected={index === activeIndex}
+            aria-controls={`tabpanel-${index}`}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+
+      <div
+        className={s.tabContent}
+        id={`tabpanel-${activeIndex}`}
+        role="tabpanel"
+        aria-labelledby={`tab-${activeIndex}`}
+      >
+        {tabs[activeIndex].content}
+      </div>
+    </div>
+  )
+}

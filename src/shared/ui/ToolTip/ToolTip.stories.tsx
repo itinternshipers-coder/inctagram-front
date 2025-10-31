65c6e2f2e2be2c076653997bc296a9b90c60672f
Vladislav <labkovich.vl@gmail.com>
2025-10-31 18:58:39 +0300

    RC-64 refactor: change stories component

--- src/shared/ui/ToolTip/ToolTip.stories.tsx
+++ src/shared/ui/ToolTip/ToolTip.stories.tsx
@@ -1,5 +1,7 @@
+import NotificationList, { notifications } from '@/shared/ui/ToolTip/NotificationList'
 import type { Meta, StoryObj } from '@storybook/nextjs-vite'
-import { ToolTip } from 'src/shared/ui/ToolTip/ToolTip'
+import ToolTip from 'src/shared/ui/ToolTip/ToolTip'
+import s from '@/shared/ui/ToolTip/ToolTip.module.scss'
 
 const meta: Meta<typeof ToolTip> = {
   title: 'Components/ToolTip',
@@ -15,43 +17,22 @@ type Story = StoryObj<typeof ToolTip>
 
 export const EmptyNotifications: Story = {
   args: {
-    notifications: [],
     unreadCount: 0,
+  },
+}
+
+export const SingleNotifications: Story = {
+  args: {
+    unreadCount: 1,
+    content: <NotificationList notifications={[notifications[0]]} />,
+    position: 'left',
   },
 }
 
 export const ManyNotifications: Story = {
   args: {
-    notifications: [
-      {
-        id: '1',
-        title: 'Новое уведомление!',
-        message: 'Следующий платеж у вас спишется через 1 день',
-        time: '1 час назад',
-        isNew: true,
-      },
-      {
-        id: '2',
-        title: 'Новое уведомление!',
-        message: 'Ваша подписка истекает через 7 дней',
-        time: '1 день назад',
-        isNew: true,
-      },
-      {
-        id: '3',
-        title: 'Новое уведомление!',
-        message: 'Ваша подписка истекает через 7 дней',
-        time: '1 день назад',
-        isNew: true,
-      },
-      {
-        id: '3',
-        title: 'Старое уведомление',
-        message: 'Ваш платеж успешно processed',
-        time: '3 дня назад',
-        isNew: false,
-      },
-    ],
     unreadCount: 3,
+    content: <NotificationList notifications={notifications} />,
+    position: 'right',
   },
 }

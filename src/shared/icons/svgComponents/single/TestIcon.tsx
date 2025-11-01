e4a58fc1b906b13d228b56d61cd58c6342f063dd
AndreyKomarov <and.kom2017@gmail.com>
2025-11-01 19:22:37 +0300

    Rc 61 (#22)
    * fix: globals.scss
    
    * add: stories for svg-components
    
    * update: preview.ts for storybook
    
    * update: add styles for any element with data-theme
    
    * update: added theme switching to storybook
    
    * fix: script to generate the components of their svg
    
    * add : new svg
    
    * update : regenerated new svgComponents
    
    * delete : SVG RecaptchaLogo
    
    * feat: add GitHub status notifications to Jenkins pipeline
    
    - Add githubNotify at pipeline start with PENDING status
    - Add post-build status notification in always{} block
    - Implement status checks for PR merging requirements
    - Context: jenkins/production-build
    
    This enables branch protection rules requiring successful Jenkins builds before merge.
    
    * feat: add GitHub status notifications to Jenkins pipeline
    
    No breaking changes to existing build/deploy logic.
    
    * feat: add GitHub status notifications to Jenkins pipeline
    
    No breaking changes to existing build/deploy logic.
    
    * feat: add GitHub status notifications to Jenkins pipeline
    
    No breaking changes to existing build/deploy logic.
    
    * Revert "feat: add GitHub status notifications to Jenkins pipeline"
    
    This reverts commit 541f4871d85f616eb511fa01c9a2652822804dbe.
    
    * Revert "feat: add GitHub status notifications to Jenkins pipeline"
    
    This reverts commit c21e5a47139ad9bca5de7421cd21a214bc2c199c.
    
    * Revert "feat: add GitHub status notifications to Jenkins pipeline"
    
    This reverts commit 0973ded740826c3df833f756af7f4b3b663da0ca.
    
    * Revert "feat: add GitHub status notifications to Jenkins pipeline"
    
    This reverts commit 2ce70b58922e3e45e8942143994b1ce3bf2d8df9.

---
+++ src/shared/icons/svgComponents/single/TestIcon.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import clsx from 'clsx'
+export type IconProps = { size?: number; color?: string; className?: string } & React.SVGProps<SVGSVGElement>
+const TestIcon = ({ size = 24, color, className, ...rest }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    className={clsx('icon', className)}
+    style={{ color }}
+    {...rest}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path d="M6 21a1 1 0 0 1-.863-.496A1 1 0 0 1 5 20V5.33A2.28 2.28 0 0 1 7.2 3h9.6A2.28 2.28 0 0 1 19 5.33V20a1 1 0 0 1-1.5.86l-5.67-3.21-5.33 3.2A1 1 0 0 1 6 21" />
+    <path d="M6.09 21.06a1 1 0 0 1-1-1L4.94 5.4a2.26 2.26 0 0 1 2.18-2.35L16.71 3a2.27 2.27 0 0 1 2.23 2.31l.14 14.66a1 1 0 0 1-.49.87 1 1 0 0 1-1 0l-5.7-3.16-5.29 3.23a1.2 1.2 0 0 1-.51.15m5.76-5.55c.174 0 .345.041.5.12l4.71 2.61-.12-12.95c0-.2-.13-.34-.21-.33l-9.6.09c-.08 0-.19.13-.19.33l.12 12.9 4.28-2.63a1.06 1.06 0 0 1 .51-.14" />
+    <path d="M14 14h-4v7h4z" />
+    <path d="M20.42 10.18 12.71 2.3a1 1 0 0 0-1.42 0l-7.71 7.89A2 2 0 0 0 3 11.62V20a2 2 0 0 0 1.89 2H8v-9a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v9h3.11A2 2 0 0 0 21 20v-8.38a2.07 2.07 0 0 0-.58-1.44" />
+    <path d="M20.42 10.18 12.71 2.3a1 1 0 0 0-1.42 0l-7.71 7.89A2 2 0 0 0 3 11.62V20a2 2 0 0 0 1.89 2h14.22A2 2 0 0 0 21 20v-8.38a2.07 2.07 0 0 0-.58-1.44M10 20v-6h4v6zm9 0h-3v-7a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H5v-8.42l7-7.15 7 7.19z" />
+    <path d="M7 6a1 1 0 0 0 0-2H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h2a1 1 0 0 0 0-2H6V6z" />
+    <path d="m20.82 11.42-2.82-4a1 1 0 1 0-1.63 1.16L18.09 11H10a1 1 0 0 0 0 2h8l-1.8 2.4a1 1 0 0 0 1.6 1.2l3-4a1 1 0 0 0 .02-1.18" />
+  </svg>
+)
+export default TestIcon

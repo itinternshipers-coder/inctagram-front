7a815b1a1050aef0cb9488f3132b4d0a03aac75a
Olga <oyakubovska80@gmail.com>
2025-11-04 14:37:45 +0100

    fix(layout): Revert unintended changes in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.scss'
-import Sidebar from '@/shared/ui/Sidebar/Sidebar'
 
 const inter = Inter({
   subsets: ['latin', 'cyrillic'],
@@ -11,10 +10,7 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.variable} data-theme="dark">
-      <body data-theme="dark">
-        {children}
-        <Sidebar role="user" />
-      </body>
+      <body data-theme="dark">{children}</body>
     </html>
   )
 }

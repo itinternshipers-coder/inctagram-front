5380c392e0e4a4d249faff9a7bf4e0403ebb29ab
Vladislav <labkovich.vl@gmail.com>
2025-11-05 16:18:53 +0300

    RC-64 refactor: change structure and style code

--- src/shared/ui/CheckBox/CheckBox.tsx
+++ src/shared/ui/CheckBox/CheckBox.tsx
@@ -18,9 +18,7 @@ export const CheckBox = ({ disabled, checked, name, onCheckedChange }: CheckBoxP
       <Checkbox.Root className={s.Root} checked={checked} disabled={disabled} onCheckedChange={onCheckedChange}>
         <Checkbox.Indicator className={s.Indicator}>{checked ? <CheckmarkOutlineIcon /> : ''}</Checkbox.Indicator>
       </Checkbox.Root>
-      <label className={disabled ? s.LabelDisabled : s.Label} htmlFor="c1">
-        {name}
-      </label>
+      <label className={disabled ? s.LabelDisabled : s.Label}>{name}</label>
     </div>
   )
 }

7d9066e7297b15a5256670e178b736c15199cfcc
AndreyKomarov <and.kom2017@gmail.com>
2025-11-07 01:48:06 +0300

    Revert "RC-62: Sidebar and Pagination (#25)" (#27)
    This reverts commit caaa33f16285c8eabe9610974bf7e90368322e1f.

--- src/shared/ui/SelectBox/SelectBox.tsx
+++ src/shared/ui/SelectBox/SelectBox.tsx
@@ -5,10 +5,10 @@ import * as Select from '@radix-ui/react-select'
 import { ComponentPropsWithoutRef } from 'react'
 import { Typography } from '../typography/Typography'
 import { clsx } from 'clsx'
-import s from './SelectBox.module.scss'
+import s from './selectBox.module.scss'
 import { ArrowBackOutlineIcon, ArrowIosDownOutlineIcon, ArrowIosUpIcon } from '@/shared/icons/svgComponents'
 
-export type Option = {
+type Option = {
   value: string
   label: string
   icon?: React.ReactNode
@@ -18,10 +18,7 @@ type Props = {
   options: Option[]
   label?: string
   placeholder?: string
-  minWidth?: string // c width на minWidth
-  height?: string
-
-  onValueChange?: (value: string) => void
+  width?: string
 } & ComponentPropsWithoutRef<typeof Select.Root>
 
 export const SelectBox = ({
@@ -29,50 +26,28 @@ export const SelectBox = ({
   options,
   defaultValue,
   placeholder = 'Select-box',
-  minWidth = '210px',
-  height = '60px', // нужная высота
-
+  width = '210px',
   disabled,
-  onValueChange,
-  value,
 }: Props) => {
   const [open, setOpen] = React.useState(false)
 
   return (
-    <div
-      className={s.wrapper}
-      // style={{ minWidth }} // применяем проп
-    >
+    <div className={s.wrapper} style={{ width }}>
       {label && (
         <Typography variant="regular_text_14" style={{ color: 'var(--light-900)' }}>
           {label}
         </Typography>
       )}
 
-      <Select.Root
-        defaultValue={defaultValue || undefined}
-        open={open}
-        onOpenChange={(isOpen) => setOpen(isOpen)}
-        onValueChange={onValueChange}
-        value={value}
-      >
+      <Select.Root defaultValue={defaultValue || undefined} open={open} onOpenChange={(isOpen) => setOpen(isOpen)}>
         <Select.Trigger
           className={clsx(s.trigger, open ? s.active : '', disabled ? s.disabled : '')}
           aria-label={placeholder}
           disabled={disabled}
-          style={{ height }} // применяем проп
         >
           <Select.Value placeholder={placeholder} />
           <Select.Icon>
-            <span className={s.arrow}>
-              {open ? (
-                // <ArrowIosUpIcon width={'16px'} height={'16px'} /> // вот тут
-                <ArrowIosUpIcon /> // вот тут
-              ) : (
-                // <ArrowIosDownOutlineIcon width={'16px'} height={'16px'} /> // вот тут
-                <ArrowIosDownOutlineIcon /> // вот тут
-              )}
-            </span>
+            <span className={s.arrow}>{open ? <ArrowIosUpIcon /> : <ArrowIosDownOutlineIcon />}</span>
           </Select.Icon>
         </Select.Trigger>
 

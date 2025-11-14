bd1a7ea25c309a1712676a23001993129ff87cb2
Komandr33 <and.kom2017@gmail.com>
2025-11-14 19:13:22 +0300

    refactor: add div for transmitting events add props

--- src/shared/ui/Switch/Switch.tsx
+++ src/shared/ui/Switch/Switch.tsx
@@ -1,23 +1,29 @@
 'use client'
 
-import ToolTip from '@/shared/ui/ToolTip/ToolTip'
-import { clsx } from 'clsx'
 import React from 'react'
 import * as Switcher from '@radix-ui/react-switch'
+import { clsx } from 'clsx'
 import s from './Switch.module.scss'
 
 type Props = {
   className?: string
   checked?: boolean
-  onCheckedChange?: (checked: boolean) => void
+  onCheckedChangeAction?: (checked: boolean) => void
+  disabled?: boolean
 }
 
-export const Switch = ({ className, checked, onCheckedChange }: Props) => {
+export const Switch = ({ className, checked, onCheckedChangeAction, disabled }: Props) => {
   return (
-    <ToolTip text={'switch theme'} openDelay={1000} position={'bottom'}>
-      <Switcher.Root className={clsx(s.root, className)} checked={checked} onCheckedChange={onCheckedChange}>
+    //<div> добавлен для передачи событий. (наведение в т.ч.)
+    <div>
+      <Switcher.Root
+        className={clsx(s.root, className)}
+        checked={checked}
+        onCheckedChange={onCheckedChangeAction}
+        disabled={disabled}
+      >
         <Switcher.Thumb className={s.thumb} />
       </Switcher.Root>
-    </ToolTip>
+    </div>
   )
 }

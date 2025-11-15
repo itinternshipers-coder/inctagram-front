57a550e0fcaae51cd38ca5db32651415ddb5b645
Olga <oyakubovska80@gmail.com>
2025-11-15 20:19:16 +0100

    add static pages

---
+++ src/shared/ui/TextPageLayout/TextPageLayout.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { ArrowBackOutlineIcon } from '@/shared/icons/svgComponents'
+import s from './TextPageLayout.module.scss'
+
+type TextPageLayoutProps = {
+  title: string
+  children: React.ReactNode
+  backHref?: string
+}
+
+const TextPageLayout = ({ title, children, backHref = '/' }: TextPageLayoutProps) => {
+  return (
+    <div className={s.container}>
+      <div className={s.backBlock}>
+        <Link href={backHref} className={s.backLink} aria-label="Назад">
+          <ArrowBackOutlineIcon className={s.arrowIcon} />
+          <span> Back to Sign Up</span>
+        </Link>
+      </div>
+
+      <div className={s.titleBar}>
+        <h1 className={s.title}>{title}</h1>
+      </div>
+
+      <div className={s.content}>{children}</div>
+    </div>
+  )
+}
+
+export default TextPageLayout

6830c658bb8be2d30337a17fec1bec42cd73e81e
Komandr33 <and.kom2017@gmail.com>
2025-11-21 02:46:27 +0300

    feat: add reducers and middleware in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,13 @@
+import authSlice from '@/features/auth/model/auth-slice'
 import { configureStore } from '@reduxjs/toolkit'
+import { baseApi } from '@/shared/api/base-api'
 
 export const store = configureStore({
   reducer: {
-    // редьюсеры
+    [baseApi.reducerPath]: baseApi.reducer,
+    [authSlice.name]: authSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>

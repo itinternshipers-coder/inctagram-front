364286bd22a2cde665133de2a974ffd338cb8f7e
ottolol <alexsveshnik@gmail.com>
2025-11-21 10:40:40 +0300

    RC-83 feat: add SignInForm component on /login page

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import SignInForm from '@/app/features/auth/ui/SignInForm/SignInForm'
+
 export default function LoginPage() {
   return (
-    <div className={''} style={{ display: 'flex' }}>
-      <h1>Login</h1>
+    <div className={''} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <SignInForm />
     </div>
   )
 }

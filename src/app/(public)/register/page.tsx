29edfa2947f3043bcab65af4d5e80065a279ffc2
Artur <metallist15431543@gmail.com>
2025-11-23 14:12:24 +0300

    RC-80 feat: add SignUpForm on registration page

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,16 +1,10 @@
+import { SignUpForm } from '@/features/auth/ui/SignUpForm/SignUpForm'
 import Link from 'next/link'
 
 export default function RegisterPage() {
   return (
-    <div className="auth-page">
-      <div className="auth-card">
-        <h1>Регистрация в Inctagram</h1>
-        <div className="auth-links">
-          <div>
-            Уже есть аккаунт? <Link href="/login">Войти</Link>
-          </div>
-        </div>
-      </div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <SignUpForm />
     </div>
   )
 }

83105f5adbd71564fb3ed218724563e469872aed
Vladislav <labkovich.vl@gmail.com>
2025-11-25 17:09:48 +0300

    RC-86 refactor: add error handling

--- src/features/auth/ui/ForgotPasswordForm/ForgotPasswordForm.tsx
+++ src/features/auth/ui/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -2,6 +2,8 @@
 import { useForgotPasswordMutation } from '@/features/auth/api/password-api'
 import { PasswordRecoveryFormData, PasswordRecoverySchema } from '@/features/auth/lib/schemas/password-recovery-schema'
 import s from '@/features/auth/ui/ForgotPasswordForm/ForgotPasswordForm.module.scss'
+import { ErrorsMessage } from '@/shared/api/types'
+import { Alerts } from '@/shared/ui/Alerts/Alerts'
 import { Button } from '@/shared/ui/Button/Button'
 import { Card } from '@/shared/ui/Card/Card'
 import { Input } from '@/shared/ui/Input/Input'
@@ -9,6 +11,8 @@ import { Modal } from '@/shared/ui/Modal/Modal'
 import { Recaptcha, RecaptchaStatus } from '@/shared/ui/Recaptcha/Recaptcha'
 import { Typography } from '@/shared/ui/Typography/Typography'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { SerializedError } from '@reduxjs/toolkit'
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
 import Link from 'next/link'
 import { useCallback, useState } from 'react'
 import { useController, useForm } from 'react-hook-form'
@@ -17,6 +21,8 @@ export const ForgotPasswordForm = () => {
   const [recaptchaStatus, setRecaptchaStatus] = useState<RecaptchaStatus>('idle')
   const [showModal, setShowModal] = useState(false)
   const [emailModal, setEmailModal] = useState('')
+  const [errorMessage, setErrorMessage] = useState<string>('')
+  const [requestSuccess, setRequestSuccess] = useState(false)
 
   const [forgotPassword] = useForgotPasswordMutation()
 
@@ -24,7 +30,7 @@ export const ForgotPasswordForm = () => {
     register,
     handleSubmit,
     control,
-    formState: { errors, isSubmitted, isValid, isSubmitting },
+    formState: { errors, isValid, isSubmitting },
     watch,
   } = useForm<PasswordRecoveryFormData>({
     resolver: zodResolver(PasswordRecoverySchema),
@@ -66,22 +72,47 @@ export const ForgotPasswordForm = () => {
   const isButtonDisabled = !isFormReady || isSubmitting
 
   const onSubmit = async (data: PasswordRecoveryFormData) => {
+    setErrorMessage('')
     try {
       // API запрос для восстановления пароля
       await forgotPassword({
         email: data.email,
         recaptchaToken: data.recaptchaToken,
-      })
-
+      }).unwrap()
       setShowModal(true)
+      setRequestSuccess(true)
       setEmailModal(data.email)
       setRecaptchaStatus('idle')
-    } catch (error) {
-      console.error('Ошибка отправки:', error)
+    } catch (err) {
+      const error = err as FetchBaseQueryError | SerializedError
+      setRecaptchaStatus('error')
+      setRequestSuccess(false)
+      if ('status' in error) {
+        const data = error.data as {
+          message?: string
+          errorsMessages?: ErrorsMessage[]
+        }
+        if (data?.message) {
+          setErrorMessage(data.message)
+        } else if (error.status === 400) {
+          setErrorMessage('Неверный формат email или ошибка reCAPTCHA')
+        } else if (error.status === 'FETCH_ERROR' || error.status === 'TIMEOUT_ERROR') {
+          setErrorMessage('Ошибка соединения с сервером. Проверьте интернет')
+        } else if (error.status === 'PARSING_ERROR') {
+          setErrorMessage('Ошибка обработки ответа сервера')
+        } else if (error.status === 500) {
+          setErrorMessage('Внутренняя ошибка сервера. Мы уже работаем над этим')
+        }
+      } else if ('message' in error) {
+        setErrorMessage(error.message || 'Произошла ошибка')
+      } else {
+        setErrorMessage('Неизвестная ошибка')
+      }
     }
   }
 
-  const buttonText = isSubmitted ? 'Send Link Again' : 'Send Link'
+  // const buttonText = !errorMessage && !isSubmitted ? 'Send Link' : 'Send Link Again'
+  const buttonText = !requestSuccess ? 'Send Link' : 'Send Link Again'
 
   return (
     <>
@@ -103,7 +134,7 @@ export const ForgotPasswordForm = () => {
               Enter your email address and we will send you further instructions
             </Typography>
 
-            {isSubmitted && (
+            {requestSuccess && (
               <Typography variant={'regular_text_14'} className={s.textForSendEmail}>
                 The link has been sent by email. If you don’t receive an email send link again
               </Typography>
@@ -118,9 +149,11 @@ export const ForgotPasswordForm = () => {
               Back to Sign In
             </Button>
           </div>
-          {/* ReCAPTCHA компонент - только со статусом */}
-          {!isSubmitted && <Recaptcha initialStatus={recaptchaStatus} onStatusChange={handleRecaptchaStatusChange} />}
+          {!requestSuccess && (
+            <Recaptcha initialStatus={recaptchaStatus} onStatusChange={handleRecaptchaStatusChange} />
+          )}
         </Card>
+        {errorMessage && <Alerts status="error" text={errorMessage} position="bottom-left" />}
       </form>
       <Modal
         open={showModal}

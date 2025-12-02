a8806da7a44f1e99c6205a10b67bf5379c5b2b64
Artur <metallist15431543@gmail.com>
2025-12-02 18:08:34 +0300

    RC-154 feat: add AuthWrapper

--- src/widgets/layout/AuthWrapper/AuthWrapper.tsx
+++ src/widgets/layout/AuthWrapper/AuthWrapper.tsx
@@ -1,3 +1,29 @@
-export const AuthWrapper = () => {
-  return <div></div>
+'use client'
+
+import React from 'react'
+import clsx from 'clsx'
+import s from './AuthWrapper.module.scss'
+import Sidebar from '@/widgets/Sidebar/Sidebar'
+import { Header } from '@/widgets/header/Header'
+import { useAuth } from '@/features/auth/lib/use-auth'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export const AuthWrapper = ({ children }: Props) => {
+  const { accessToken } = useAuth()
+  const isLoggedIn = !!accessToken
+
+  return (
+    <div>
+      <Header isLoginIn={isLoggedIn} />
+      <div className={s.contentWrapper}>
+        {isLoggedIn && <Sidebar role="user" />}
+        <div className={s.contentBlock}>
+          <div className={clsx(s.children, !isLoggedIn && s.childrenWithoutSidebar)}>{children}</div>
+        </div>
+      </div>
+    </div>
+  )
 }

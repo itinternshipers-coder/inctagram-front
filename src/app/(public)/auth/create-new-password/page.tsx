5fb7ba5a913ff99d57f49266cd19652ec09f3cd5
Komandr33 <and.kom2017@gmail.com>
2025-12-04 18:43:20 +0300

    feat: add create-new-password page

---
+++ src/app/(public)/auth/create-new-password/page.tsx
@@ -0,0 +1,9 @@
+import { CreateNewPassword } from '@/features/auth/ui/CreateNewPassword/CreateNewPassword'
+
+export default function CreateNewPasswordPage() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <CreateNewPassword />
+    </div>
+  )
+}

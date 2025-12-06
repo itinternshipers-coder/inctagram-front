45c8935aa61f4dc042b0481cf42b555282f6b661
Komandr33 <and.kom2017@gmail.com>
2025-12-06 15:00:36 +0300

    refactor: delete page create-new-password

--- src/app/(public)/auth/create-new-password/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateNewPassword } from '@/features/auth/ui/CreateNewPassword/CreateNewPassword'
-
-export default function CreateNewPasswordPage() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-      <CreateNewPassword />
-    </div>
-  )
-}

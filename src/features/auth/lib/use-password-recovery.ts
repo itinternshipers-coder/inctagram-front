50d689e421eede527e8a9946b98cd4ffd216fd03
Komandr33 <and.kom2017@gmail.com>
2025-12-06 15:07:47 +0300

    feat: add hook usePasswordRecovery

---
+++ src/features/auth/lib/use-password-recovery.ts
@@ -0,0 +1,30 @@
+'use client'
+
+import { useVerifyRecoveryCodeQuery } from '@/features/auth/api/password-api'
+import { normalizeError } from '@/shared/api/error-utils'
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export const usePasswordRecovery = () => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const recoveryCode = searchParams.get('code') || ''
+  const email = searchParams.get('email') || ''
+
+  const { data, isLoading, error } = useVerifyRecoveryCodeQuery({ code: recoveryCode }, { skip: !recoveryCode })
+
+  useEffect(() => {
+    if (!recoveryCode) {
+      router.replace('/custom-error?message=Missing recovery code')
+    }
+  }, [recoveryCode])
+
+  return {
+    recoveryCode,
+    email,
+    data,
+    isLoading,
+    error: normalizeError(error),
+  }
+}

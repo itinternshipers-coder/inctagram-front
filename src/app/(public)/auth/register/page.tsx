0290c97022c3cc4b6be6d42cf223bbd37d321b4f
Komandr33 <and.kom2017@gmail.com>
2025-12-10 23:34:59 +0300

    fix: delete unused pages

--- src/app/(public)/auth/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUpForm } from '@/features/auth/ui/SignUpForm/SignUpForm'
-
-export default function RegisterPage() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-      <SignUpForm />
-    </div>
-  )
-}

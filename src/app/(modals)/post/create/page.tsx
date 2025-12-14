8fb1eb468a329d2f6be3a5c793b453ffd3f2293e
Vladislav <labkovich.vl@gmail.com>
2025-12-14 17:50:14 +0300

    RC-155 feat: add routing for CreatePostModal component

---
+++ src/app/(modals)/post/create/page.tsx
@@ -0,0 +1,9 @@
+import { CreatePostModal } from '@/widgets/CreatPost/CreatePostModal/CreatePostModal'
+
+export default function CreatePostModalPage() {
+  return (
+    <>
+      <CreatePostModal />
+    </>
+  )
+}

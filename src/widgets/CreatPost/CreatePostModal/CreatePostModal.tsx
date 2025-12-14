aa19210e63643a73ba5d3aa99ffde6f3f85f6430
Vladislav <labkovich.vl@gmail.com>
2025-12-14 17:12:34 +0300

    RC-155 refactor: change structure component CreatePostModal

--- src/widgets/CreatPost/CreatePostModal/CreatePostModal.tsx
+++ src/widgets/CreatPost/CreatePostModal/CreatePostModal.tsx
@@ -1,83 +1,58 @@
 'use client'
-import { CloseOutlineIcon } from '@/shared/icons/svgComponents'
 import { AddPhoto } from '@/widgets/CreatPost/CreatePostModal/AddPhoto/AddPhoto'
+import { Cropping } from '@/widgets/CreatPost/CreatePostModal/Cropping/Cropping'
+import { ModalHeader } from '@/widgets/CreatPost/CreatePostModal/ModalHeader'
+import { useModalSteps } from '@/widgets/CreatPost/CreatePostModal/useModalSteps'
 import React, { useState } from 'react'
 import s from './CreatePostModal.module.scss'
 
-// type CreatePostModalProps = {}
-
-type ModalStep = 'add-photo' | 'cropping' | 'filters' | 'publication'
+export type ModalStep = 'add-photo' | 'cropping' | 'filters' | 'publication'
 
 export const CreatePostModal = () => {
-  const [currentStep, setCurrentStep] = useState<ModalStep>('add-photo')
-  // const [selectedImage, setSelectedImage] = useState<File | null>(null)
+  const [selectedImage, setSelectedImage] = useState<File | null>(null)
   const [isOpen, setIsOpen] = useState(true)
 
-  // Берется из хука useImageUpload.ts
+  const { currentStep, goNext, goBack } = useModalSteps()
 
-  // const [croppedImage, setCroppedImage] = useState<string>('')
+  const [croppedImage, setCroppedImage] = useState<File | null>(null)
   // const [filteredImage, setFilteredImage] = useState<string>('')
 
-  // Навигация
-  // const goNext = () => {
-  //   // Логика перехода на следующий шаг
-  //   if (currentStep === 'add-photo') setCurrentStep('cropping')
-  //   else if (currentStep === 'cropping') setCurrentStep('filters')
-  //   else if (currentStep === 'filters') setCurrentStep('publication')
-  // }
-  // const goBack = () => {
-  //   // Логика возврата на предыдущий шаг
-  //   if (currentStep === 'cropping') setCurrentStep('add-photo')
-  //   else if (currentStep === 'filters') setCurrentStep('cropping')
-  //   else if (currentStep === 'publication') setCurrentStep('filters')
-  // }
-
-  // Загрузку и Валидацию изображения берем из хука useImageUpload.ts
-  // // Обработчик выбора изображения
+  // Обработчик выбора изображения
   const handleImageSelect = (file: File | null) => {
-    // setSelectedImage(file)
+    setSelectedImage(file)
     // Автоматически переходим к следующему шагу при успешной загрузке
-    setCurrentStep('cropping')
+    goNext()
   }
+
   // Обработчик закрытия модалки
-
   const handleCloseModal = () => {
-    setIsOpen(!isOpen)
+    setIsOpen(false)
   }
 
-  const stepTitles: Record<ModalStep, string> = {
-    'add-photo': 'Add Photo',
-    cropping: 'Cropping',
-    filters: 'Apply Filters',
-    publication: 'Create Post',
+  // Обработчик публикации
+  const handlePublish = () => {
+    console.log('Publishing:', { selectedImage, croppedImage })
+    // Здесь логика публикации
+    handleCloseModal()
   }
 
   if (!isOpen) return null
 
   return (
-    <div className={s.overlay}>
-      <div className={s.containerCard}>
-        <div className={s.cardHeader}>
-          <div className={s.title}>{stepTitles[currentStep]}</div>
-          <div className={s.closeBtn} onClick={handleCloseModal}>
-            <CloseOutlineIcon />
-          </div>
-        </div>
-        <div className={s.contentCard}>
+    <div className={s.overlayModal} onClick={handleCloseModal}>
+      <div className={s.containerModal} onClick={(e) => e.stopPropagation()}>
+        <ModalHeader
+          currentStep={currentStep}
+          onBack={goBack}
+          onNext={currentStep === 'publication' ? handlePublish : goNext}
+          onClose={handleCloseModal}
+        />
+        <div className={s.contentModal}>
           {currentStep === 'add-photo' && <AddPhoto onSelectImage={handleImageSelect} />}
+          {currentStep === 'cropping' && <Cropping image={selectedImage} onCropComplete={setCroppedImage} />}
+          {/*{currentStep === 'filters' && <Filters image={croppedImage} onFilterApply={setFilteredImage} />}*/}
+          {/*{currentStep === 'publication' && <Publication image={filteredImage} onBack={goBack} onPublish={handlePublish} />}*/}
         </div>
-
-        {/*{currentStep === 'cropping' && (*/}
-        {/*  <Cropping image={selectedImage} onCropComplete={setCroppedImage} onBack={goBack} onNext={goNext} />*/}
-        {/*)}*/}
-
-        {/*{currentStep === 'filters' && (*/}
-        {/*  <Filters image={croppedImage} onFilterApply={setFilteredImage} onBack={goBack} onNext={goNext} />*/}
-        {/*)}*/}
-
-        {/*{currentStep === 'publication' && (*/}
-        {/*  <Publication image={filteredImage} onBack={goBack} onPublish={handlePublish} />*/}
-        {/*)}*/}
       </div>
     </div>
   )

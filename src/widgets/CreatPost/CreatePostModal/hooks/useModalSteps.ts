cb5881ecb87e35a334ece9f9aa9f035211fdd57a
Vladislav <labkovich.vl@gmail.com>
2025-12-18 10:54:49 +0300

    RC-155 feat: add hook useModalSteps

---
+++ src/widgets/CreatPost/CreatePostModal/hooks/useModalSteps.ts
@@ -0,0 +1,23 @@
+import { ModalStep } from '@/widgets/CreatPost/CreatePostModal/CreatePostModal'
+import { useState } from 'react'
+
+export const useModalSteps = () => {
+  const [currentStep, setCurrentStep] = useState<ModalStep>('add-photo')
+
+  const modalSteps = ['add-photo', 'cropping', 'filters', 'publication'] as const
+
+  const goNext = () => {
+    const currentIndex = modalSteps.indexOf(currentStep)
+    if (currentIndex < modalSteps.length - 1) {
+      setCurrentStep(modalSteps[currentIndex + 1])
+    }
+  }
+
+  const goBack = () => {
+    const currentIndex = modalSteps.indexOf(currentStep)
+    if (currentIndex > 0) {
+      setCurrentStep(modalSteps[currentIndex - 1])
+    }
+  }
+  return { currentStep, goNext, goBack }
+}

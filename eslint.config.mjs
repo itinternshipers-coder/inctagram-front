8d4020145e36b4275ac154cf9bb4a45f70e6b721
Komandr33 <and.kom2017@gmail.com>
2025-12-18 18:35:17 +0300

    chore(lint): migrate eslint config to flat format and use FlatCompat for storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,38 @@
-// For more info: https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-
-import storybook from 'eslint-plugin-storybook'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import { createRequire } from 'module'
 import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const require = createRequire(import.meta.url)
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
-const eslintConfig = [
-  // Базовые конфиги Next.js + TypeScript + Prettier
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier', 'plugin:storybook/recommended'),
 
-  // Игнорируем служебные директории
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
 
-  // Рекомендованные правила Storybook
-  ...storybook.configs['flat/recommended'],
-
-  // Специальные исключения для Storybook файлов
   {
-    files: ['src/stories/**/*'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'storybook/hierarchy-separator': 'off',
-      'storybook/no-redundant-story-name': 'error',
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: require.resolve('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
     },
-  },
-
-  // Основные правила для всего проекта
-  {
+    plugins: {
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      storybook: require('eslint-plugin-storybook'),
+    },
     rules: {
-      // Разрешаем неиспользуемые переменные если они начинаются с _
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
@@ -50,12 +44,20 @@ const eslintConfig = [
     },
   },
 
-  // Исключения и глобалы для тестов (Vitest, Playwright)
+  {
+    files: ['src/stories/**/*'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'storybook/hierarchy-separator': 'off',
+      'storybook/no-redundant-story-name': 'error',
+    },
+  },
+
   {
     files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}', '**/tests/**/*.{ts,tsx}'],
     languageOptions: {
       globals: {
-        // Vitest globals
         vi: true,
         describe: true,
         it: true,
@@ -65,7 +67,6 @@ const eslintConfig = [
         beforeEach: true,
         afterAll: true,
         afterEach: true,
-        // Playwright globals
         page: true,
         browser: true,
         context: true,
@@ -78,5 +79,3 @@ const eslintConfig = [
     },
   },
 ]
-
-export default eslintConfig

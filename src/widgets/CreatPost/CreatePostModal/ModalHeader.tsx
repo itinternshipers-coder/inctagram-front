be37d916c350266e5e1a0327888b7e9367aae5b7
Vladislav <labkovich.vl@gmail.com>
2025-12-18 10:55:27 +0300

    RC-155 feat: add component ModalHeader

---
+++ src/widgets/CreatPost/CreatePostModal/ModalHeader.tsx
@@ -0,0 +1,52 @@
+import { ArrowIosBackOutlineIcon, CloseOutlineIcon } from '@/shared/icons/svgComponents'
+import { Button } from '@/shared/ui/Button/Button'
+import { Typography } from '@/shared/ui/Typography/Typography'
+import { ModalStep } from '@/widgets/CreatPost/CreatePostModal/CreatePostModal'
+import Link from 'next/link'
+import s from './CreatePostModal.module.scss'
+
+export const ModalHeader = ({
+  currentStep,
+  onBack,
+  onNext,
+  onClose,
+}: {
+  currentStep: ModalStep
+  onBack: () => void
+  onNext: () => void
+  onClose: () => void
+}) => {
+  const stepTitles: Record<ModalStep, string> = {
+    'add-photo': 'Add Photo',
+    cropping: 'Cropping',
+    filters: 'Filters',
+    publication: 'Publication',
+  }
+
+  return (
+    <div className={s.headerModal}>
+      {currentStep === 'add-photo' ? (
+        <>
+          <Typography variant={'h1'}>{stepTitles[currentStep]}</Typography>
+          <div className={s.closeBtn} onClick={onClose}>
+            <CloseOutlineIcon />
+          </div>
+        </>
+      ) : (
+        <>
+          <ArrowIosBackOutlineIcon onClick={onBack} className={s.buttonBack} />
+          <Typography variant={'h1'}>{stepTitles[currentStep]}</Typography>
+          <Button variant={'link'} as={'div'} onClick={onNext}>
+            {currentStep === 'publication' ? (
+              <Button as={Link} href={''} variant={'link'}>
+                {'Publish'}
+              </Button>
+            ) : (
+              'Next'
+            )}
+          </Button>
+        </>
+      )}
+    </div>
+  )
+}

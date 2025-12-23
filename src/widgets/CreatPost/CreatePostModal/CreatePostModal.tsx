4ec175eb5c79c656c45db9f11deafc2c079e7670
Vladislav <labkovich.vl@gmail.com>
2025-12-23 21:33:34 +0300

    RC-155 refactor: change structure CreatePostModal component

--- src/widgets/CreatPost/CreatePostModal/CreatePostModal.tsx
+++ src/widgets/CreatPost/CreatePostModal/CreatePostModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { AddPhoto } from '@/widgets/CreatPost/CreatePostModal/AddPhoto/AddPhoto'
 import { Cropping } from '@/widgets/CreatPost/CreatePostModal/Cropping/Cropping'
-import { ModalHeader } from '@/widgets/CreatPost/CreatePostModal/ModalHeader'
+import { Filters } from '@/widgets/CreatPost/CreatePostModal/Filters/Filters'
 import { useModalSteps } from '@/widgets/CreatPost/CreatePostModal/hooks/useModalSteps'
 import React, { useState } from 'react'
 import s from './CreatePostModal.module.scss'
@@ -11,11 +11,10 @@ export type ModalStep = 'add-photo' | 'cropping' | 'filters' | 'publication'
 export const CreatePostModal = () => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null)
   const [isOpen, setIsOpen] = useState(true)
+  const [croppedImage, setCroppedImage] = useState<File | null>(null)
+  const [filteredImage, setFilteredImage] = useState<File | null>(null)
 
   const { currentStep, goNext, goBack } = useModalSteps()
-
-  const [croppedImage, setCroppedImage] = useState<File | null>(null)
-  // const [filteredImage, setFilteredImage] = useState<string>('')
 
   // Обработчик выбора изображения
   const handleImageSelect = (file: File | null) => {
@@ -30,30 +29,56 @@ export const CreatePostModal = () => {
   }
 
   // Обработчик публикации
-  const handlePublish = () => {
-    console.log('Publishing:', { selectedImage, croppedImage })
-    // Здесь логика публикации
-    handleCloseModal()
+  // const handlePublish = () => {
+  //   console.log('Publishing:', { selectedImage, croppedImage })
+  //   // Здесь логика публикации
+  //   handleCloseModal()
+  // }
+
+  // Обработчик обработанного изображения
+  const handleCropComplete = (croppedFile: File) => {
+    setCroppedImage(croppedFile)
   }
 
   if (!isOpen) return null
 
   return (
     <div className={s.overlayModal} onClick={handleCloseModal}>
-      <div className={s.containerModal} onClick={(e) => e.stopPropagation()}>
-        <ModalHeader
-          currentStep={currentStep}
-          onBack={goBack}
-          onNext={currentStep === 'publication' ? handlePublish : goNext}
-          onClose={handleCloseModal}
-        />
-        <div className={s.contentModal}>
-          {currentStep === 'add-photo' && <AddPhoto onSelectImage={handleImageSelect} />}
-          {currentStep === 'cropping' && <Cropping image={selectedImage} onCropComplete={setCroppedImage} />}
-          {/*{currentStep === 'filters' && <Filters image={croppedImage} onFilterApply={setFilteredImage} />}*/}
-          {/*{currentStep === 'publication' && <Publication image={filteredImage} onBack={goBack} onPublish={handlePublish} />}*/}
+      {currentStep === 'add-photo' || currentStep === 'cropping' ? (
+        <div className={s.containerModalSquare} onClick={(e) => e.stopPropagation()}>
+          {currentStep === 'add-photo' && (
+            <AddPhoto onSelectImage={handleImageSelect} onCloseModal={handleCloseModal} />
+          )}
+          {currentStep === 'cropping' && (
+            <Cropping
+              image={selectedImage}
+              onCropComplete={handleCropComplete}
+              currentStep={currentStep}
+              onNext={goNext}
+              onBack={goBack}
+            />
+          )}
         </div>
-      </div>
+      ) : (
+        <div className={s.containerModalRectangular} onClick={(e) => e.stopPropagation()}>
+          {currentStep === 'filters' && (
+            <Filters
+              image={croppedImage}
+              onFilterApply={setFilteredImage}
+              currentStep={currentStep}
+              onNext={goNext}
+              onBack={goBack}
+            />
+          )}
+          {/*{currentStep === 'publication' && (*/}
+          {/*  <Publication*/}
+          {/*    image={filteredImage}*/}
+          {/*    onBack={goBack}*/}
+          {/*    onPublish={handlePublish}*/}
+          {/*  />*/}
+          {/*)}*/}
+        </div>
+      )}
     </div>
   )
 }

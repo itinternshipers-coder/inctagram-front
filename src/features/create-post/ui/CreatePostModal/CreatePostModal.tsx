dd925ab5944e7dbf2264a25f9dc25f3ed09f3290
Vladislav <labkovich.vl@gmail.com>
2026-01-19 15:20:42 +0300

    RC-155 refactor: change imports in component CreatePostModal

--- src/features/create-post/ui/CreatePostModal/CreatePostModal.tsx
+++ src/features/create-post/ui/CreatePostModal/CreatePostModal.tsx
@@ -1,13 +1,11 @@
 'use client'
 
-import { AddPhoto } from '@/features/create-post/ui/CreatePostModal/AddPhoto/AddPhoto'
-import { Cropping } from '@/features/create-post/ui/CreatePostModal/Cropping/Cropping'
-import { Filters } from '@/features/create-post/ui/CreatePostModal/Filters/Filters'
+import { AddPhoto } from './AddPhoto/AddPhoto'
+import { Cropping } from './Cropping/Cropping'
+import { Filters } from './Filters/Filters'
 import { useModalSteps } from '@/features/create-post/model/lib/useModalSteps'
-import { Publication } from '@/features/create-post/ui/CreatePostModal/Publication/Publication'
+import { Publication } from './Publication/Publication'
 import React, { useState } from 'react'
-
-export type ModalStep = 'add-photo' | 'cropping' | 'filters' | 'publication'
 
 export const CreatePostModal = () => {
   const [isOpen, setIsOpen] = useState(true)

4d9099112c7aed4bef612c503f11f3113a120919
Artur <metallist15431543@gmail.com>
2026-01-30 15:33:35 +0300

    RC-203 fix: add missing space in 'characters 254' message

--- src/features/auth/lib/schemas/field-schemas.ts
+++ src/features/auth/lib/schemas/field-schemas.ts
@@ -10,7 +10,7 @@ export const UserNameSchema = z
 export const EmailSchema = z
   .string()
   .min(1, 'Email is required')
-  .max(254, 'Maximum number of characters254')
+  .max(254, 'Maximum number of characters 254')
   .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'The email must match the format example@example.com')
 
 export const PasswordSchema = z

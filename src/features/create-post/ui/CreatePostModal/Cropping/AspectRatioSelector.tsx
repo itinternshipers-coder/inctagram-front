03aacc25ad4ede670a22ef2ec61e19cd97df2a53
Vladislav <labkovich.vl@gmail.com>
2026-01-31 16:30:16 +0300

    RC-155 refactor: add component AspectRatioSelector

---
+++ src/features/create-post/ui/CreatePostModal/Cropping/AspectRatioSelector.tsx
@@ -0,0 +1,29 @@
+import { ASPECT_RATIO_OPTIONS } from '@/features/create-post/ui/CreatePostModal/Cropping/constants'
+import { AspectRatio } from './types'
+import s from './Cropping.module.scss'
+
+type AspectRatioSelectorProps = {
+  selectedAspect: AspectRatio
+  onChange: (ratio: AspectRatio) => void
+}
+
+export const AspectRatioSelector = ({ selectedAspect, onChange }: AspectRatioSelectorProps) => {
+  return (
+    <div className={s.aspectSection}>
+      <div className={s.aspectTitle}>Aspect Ratio</div>
+      <div className={s.aspectButtons}>
+        {ASPECT_RATIO_OPTIONS.map((ratio) => (
+          <button
+            key={ratio.label}
+            className={`${s.aspectButton} ${selectedAspect.value === ratio.value ? s.active : ''}`}
+            onClick={() => {
+              onChange(ratio)
+            }}
+          >
+            {ratio.label}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}

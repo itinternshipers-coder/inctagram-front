aff018678c8bab088bef72d6d3f87bc5bede2dd2
NovikovRomanV <rimm19845@gmail.com>
2026-02-07 22:26:02 +0300

    feat: added a page for modal and full-screen display of the publication; del: deleted the modal folder and page.

---
+++ src/app/post/[postId]/page.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import s from './page.module.scss'
+import { PostView } from '@/shared/ui/PostView/PostView'
+
+type PageProps = {
+  params: Promise<{ postId: string }>
+}
+
+export default async function PostPage({ params }: PageProps) {
+  const { postId } = await params
+
+  return (
+    <div className={s.postModal}>
+      <Link href={'/'} className={s.linkBack}>
+        Back
+      </Link>
+      <PostView postId={postId} />
+    </div>
+  )
+}

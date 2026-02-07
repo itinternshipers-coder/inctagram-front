b368f90fff3228718ab9cddce6e1e89064e5b771
NovikovRomanV <rimm19845@gmail.com>
2026-02-07 22:51:14 +0300

    fix: PostModal.tsx, PostEdit.tsx.

--- src/shared/ui/PostModal/PostEdit/PostEdit.tsx
+++ src/shared/ui/PostModal/PostEdit/PostEdit.tsx
@@ -1,4 +1,4 @@
-import { Author } from '../PostModal'
+import { Author } from '@/features/post/model/type'
 import * as Dialog from '@radix-ui/react-dialog'
 
 import s from '../PostModal.module.scss'
@@ -7,7 +7,7 @@ import { CloseOutlineIcon } from '@/shared/icons/svgComponents'
 import TextArea from '../../TextArea/TextArea'
 import { useState } from 'react'
 import { useAppDispatch } from '@/shared/lib/hooks'
-import { openEditModal, Post } from '@/entities/post/model'
+import { openEditModal } from '@/entities/post/model'
 
 type PostEditHeaderProps = {
   author: Author

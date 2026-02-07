258702483f7c58470da73df870c3ca9abdd2e05d
NovikovRomanV <rimm19845@gmail.com>
2026-02-07 22:29:04 +0300

    feat: added PostModalClient.tsx, page.module.scss, default.tsx.

---
+++ src/app/@modal/(.)post/[postId]/PostModalClient.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useGetPostByIdQuery } from '@/entities/post/api/posts-api'
+import PostModal from '@/shared/ui/PostModal/PostModal'
+import { useParams, useRouter } from 'next/navigation'
+
+export default function PostModalClient() {
+  const { postId } = useParams<{ postId: string }>()
+  const { data, isLoading, error } = useGetPostByIdQuery({ id: postId })
+  const router = useRouter()
+  if (isLoading) return null
+  if (error || !data) return null
+
+  return <PostModal postData={data} open={true} onOpenChange={() => router.back()} comments={[]} />
+}

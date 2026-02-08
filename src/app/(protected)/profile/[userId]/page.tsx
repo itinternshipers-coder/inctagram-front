0cc59450cf5ebb5a0c2d58ecc1122b93b76ff634
Komandr33 <and.kom2017@gmail.com>
2026-02-08 20:50:25 +0300

    feat(app): implement SSR profile page with dynamic data (temporary data output)

--- src/app/(protected)/profile/[userId]/page.tsx
+++ src/app/(protected)/profile/[userId]/page.tsx
@@ -1,19 +1,74 @@
-'use client'
+import { Post } from '@/entities/post/model'
+import { fetchProfileData } from '@/shared/lib/server/fetch-profile-data'
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
 
-import { use } from 'react'
+export default async function ProfilePage({ params }: { params: Promise<{ userId: string }> }) {
+  const { userId } = await params
 
-type ProfilePageProps = {
-  params: Promise<{ userId: string }>
-  searchParams: Promise<{ _postId?: string; _action?: string }>
-}
+  if (!userId || userId === 'undefined') {
+    console.error('Invalid userId:', userId)
+    notFound()
+  }
 
-export default function ProfilePage({ params, searchParams }: ProfilePageProps) {
-  const { userId } = use(params)
-  const { _postId, _action } = use(searchParams)
+  const { profile, posts } = await fetchProfileData(userId)
+
+  if (!profile) {
+    console.log('Profile not found for userId:', userId)
+    notFound()
+  }
 
   return (
     <div>
-      <h1>Профиль пользователя {userId}</h1>
+      <div>
+        <div>
+          {profile.avatarUrl ? (
+            <Image src={profile.avatarUrl} alt={profile.userName} />
+          ) : (
+            <div className="avatar-placeholder">{profile.userName?.charAt(0)?.toUpperCase() || 'Photo is missing'}</div>
+          )}
+        </div>
+
+        <div>
+          <h1>{`UserName: ${profile.username}` || 'Unknown User'}</h1>
+          <h1>{`UserID: ${profile.userId}` || 'Unknown ID'}</h1>
+
+          <div>
+            <div>
+              <strong>{profile.postsCount || 0}</strong>
+              <span> posts</span>
+            </div>
+            <div>
+              <strong>{profile.followersCount || 0}</strong>
+              <span> followers</span>
+            </div>
+            <div>
+              <strong>{profile.followingCount || 0}</strong>
+              <span> following</span>
+            </div>
+          </div>
+
+          {profile.description && <p className="profile-description">{profile.description}</p>}
+        </div>
+      </div>
+
+      <div>
+        {posts.length > 0 ? (
+          posts.map((post: Post) => (
+            <div key={post.id}>
+              {post.photos?.[0]?.url ? (
+                <img src={post.photos[0].url} alt={post.description || 'Post'} />
+              ) : (
+                <div>
+                  <span>{post.description?.substring(0, 50) || 'No description'}...</span>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <p>No posts yet</p>
+        )}
+      </div>
     </div>
   )
 }

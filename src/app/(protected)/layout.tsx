e428835662cc696e1246bb8563101b616ac60a56
Komandr33 <and.kom2017@gmail.com>
2026-02-08 20:52:20 +0300

    refactor(app): delete unused code

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,11 +1,6 @@
-// import { Header } from '@/widgets/header/Header'
-// import { Sidebar } from '@/widgets/sidebar/Sidebar'
-
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="protected-layout">
-      {/*<Header />*/}
-      {/*<Sidebar />*/}
       <main className="main-content">{children}</main>
     </div>
   )

83d182a17e3a6e7fa310689ac9d2543b0c92a818
Komandr33 <and.kom2017@gmail.com>
2026-02-15 20:42:19 +0300

    fix: delete duplication property images

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.yandexcloud.net',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-
   async rewrites() {
     return [
       {

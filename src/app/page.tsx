c07de20dfeaa4a3b9861d3c538de7690185d7838
Komandr33 <and.kom2017@gmail.com>
2026-05-28 14:47:22 +0300

    feat: add HomeContent wrapper for conditional feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,18 @@ import { PostList } from '@/entities/post/ui/PostList/PostList'
 import { getMainPageData } from '@/features/userStats/helpers/getMainPageData'
 import { MainPageError } from '@/features/userStats/ui/MainPageError/MainPageError'
 import { RegisteredUsers } from '@/features/userStats/ui/RegisteredUsers/RegisteredUsers'
+import { HomeContent } from './HomeContent' // клиентский компонент
 
 export default async function RootPage() {
   const data = await getMainPageData()
+  if (!data.ok) return <MainPageError />
 
-  if (!data.ok) {
-    return <MainPageError />
-  }
-
-  return (
-    <div>
+  const guestContent = (
+    <>
       <RegisteredUsers count={data.usersCount} />
       <PostList posts={data.recentPosts} />
-    </div>
+    </>
   )
+
+  return <HomeContent guestContent={guestContent} />
 }
